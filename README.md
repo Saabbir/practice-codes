1c9908ae31facc6565d9e80b194fe9bfadbe1b2d
Saabbir Hossain <thesaabbir@gmail.com>
2023-09-06 04:14:28 +0600

    Create StopWatch constructor function

---
+++ README.md
@@ -0,0 +1,1 @@
+The repository's main purpose is to keep practice code written while taking a course or reading books or articles.
---
+++ courses/mosh-hamedani-oop-in-js/StopWatch.js
@@ -0,0 +1,34 @@
+/**
+ * Exercise - Stop Watch
+ */
+
+function StopWatch() {
+  let intervalId;
+  let running = false;
+
+  this.duration = 0;
+
+  this.start = () => {
+    if (running) {
+      throw new Error("Stopwatch already started");
+    }
+    running = true;
+    intervalId = setInterval(() => {
+      this.duration = this.duration + 1;
+    }, 1);
+  };
+
+  this.stop = () => {
+    if (running) {
+      clearInterval(intervalId);
+      running = false;
+    } else {
+      throw new Error("Stopwatch is not running!");
+    }
+  };
+
+  this.reset = () => {
+    this.duration = 0;
+    running = false;
+  };
+}
